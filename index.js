353dad70419339203d31ed473365efab7f777366
Javier Parales <alejito09parales@gmail.com>
2024-11-07 14:20:17 -0500

    commit2

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express'); 
+const colors = require('colors'); 
+const app = express(); 
+const path = require('path'); 
+
+app.use(express.urlencoded({ extended: false })); 
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'inicio.html'));
+app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname, 'views')); 
+
+// Middleware para servir archivos estáticos (imágenes, hojas de estilo CSS, scripts JS)
+app.use(express.static(path.join(__dirname, 'public'))); // Sirve los archivos estáticos desde la carpeta 'public'
+
+
+
+// Ruta raíz (/) que redirige automáticamente a la página '/Inicio'
+app.get('/', (req, res) => res.redirect('/Inicio'));
+
+
+app.get('/Inicio', (req, res) => {
+    console.log("Accediendo a la página de login".bgBlue); 
+    res.render('index'); 
 });
+
 
 app.get('/Datos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Datos.html'));
+    console.log("Accediendo a la página de registro".bgMagenta);
+    res.render('Datos'); 
 });
+
 
 app.get('/Bienvenido', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Bienvenido.html'));
+    const userName = "Usuario"; 
+    console.log("Accediendo a la página de bienvenida".bgRed); 
+    res.render('Biendenido', { userName });
 });
 
-app.listen(3030, () => {
-    console.log("Servidor en funcionamiento en la url");
-});
+// Configura el puerto en el que el servidor escuchará las solicitudes HTTP
+const puerto = 3030; 
+console.log(`Servidor en el puerto ${puerto}`.bgGreen); 
+app.listen(puerto); 
